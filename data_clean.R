edca541eed8363ded7e26a63b9bb840d8d9a7423
Hantang Zhou <htmajorcn@hotmail.com>
2018-12-08 15:36:44 -0500

    Removed Invalide Zipcode

--- data_clean.R
+++ data_clean.R
@@ -1,17 +1,30 @@
 library(tidyverse)
 setwd("/Users/hantangzhou/OneDrive/Columbia University in the City of New York/STAT GR5702/Final Project/edav_project")
-#data <- read_csv('Rodent_Inspection.csv')
+data <- read_csv('Rodent_Inspection.csv')
 
-#data <- data %>% select(-JOB_TICKET_OR_WORK_ORDER_ID, -JOB_ID, -LOCATION, -JOB_PROGRESS, -BOROUGH, -X_COORD, 
-#                        -Y_COORD, -APPROVED_DATE, -ID)
+clean = 0
+if(clean == 1){
+  data <- data %>% select(-JOB_TICKET_OR_WORK_ORDER_ID, -JOB_ID, -LOCATION, -JOB_PROGRESS, -BOROUGH, -X_COORD, 
+                        -Y_COORD, -APPROVED_DATE, -ID)
 
-#data <- data %>% select(-BBL, -LOT, -BLOCK, -HOUSE_NUMBER, -STREET_NAME, -RESULT)
+  data <- data %>% select(-BBL, -LOT, -BLOCK, -HOUSE_NUMBER, -STREET_NAME, -RESULT)
 
-#data <- data %>% drop_na()
+  data <- na_if(data, 0)
+  data <- na_if(data, 111111)
 
-#data <- tibble::rowid_to_column(data, "ID")
+  data <- data %>% drop_na()
 
-#sample <- sample_n(data, 0.2 * nrow(data))
+  data <- tibble::rowid_to_column(data, "ID")
 
-#write_csv(sample, "Rodent_Inspection_Lite.csv")
+  sample <- sample_n(data, 0.2 * nrow(data))
 
+  write_csv(sample, "Rodent_Inspection_Lite.csv")
+}
+
+createFunction <- function(data){
+  
+  #for(i in){
+    
+  #}
+  
+}
