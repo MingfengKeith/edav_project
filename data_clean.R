9d737a646bcd42abdcf83975073b730959a12275
Hantang Zhou <htmajorcn@hotmail.com>
2018-12-08 15:24:28 -0500

    Making a new folder for date

--- data_clean.R
+++ data_clean.R
@@ -1,16 +1,17 @@
 library(tidyverse)
 setwd("/Users/hantangzhou/OneDrive/Columbia University in the City of New York/STAT GR5702/Final Project/edav_project")
-data <- read_csv('Rodent_Inspection.csv')
+#data <- read_csv('Rodent_Inspection.csv')
 
-data <- data %>% select(-JOB_TICKET_OR_WORK_ORDER_ID, -JOB_ID, -LOCATION, -JOB_PROGRESS, -BOROUGH, -X_COORD, 
-                        -Y_COORD, -APPROVED_DATE, -ID)
+#data <- data %>% select(-JOB_TICKET_OR_WORK_ORDER_ID, -JOB_ID, -LOCATION, -JOB_PROGRESS, -BOROUGH, -X_COORD, 
+#                        -Y_COORD, -APPROVED_DATE, -ID)
 
-data <- data %>% select(-BBL, -LOT, -BLOCK, -HOUSE_NUMBER, -STREET_NAME, -RESULT)
+#data <- data %>% select(-BBL, -LOT, -BLOCK, -HOUSE_NUMBER, -STREET_NAME, -RESULT)
 
-data <- data %>% drop_na()
+#data <- data %>% drop_na()
 
-data <- tibble::rowid_to_column(data, "ID")
+#data <- tibble::rowid_to_column(data, "ID")
 
-sample <- sample_n(data, 0.2 * nrow(data))
+#sample <- sample_n(data, 0.2 * nrow(data))
 
-write_csv(sample, "Rodent_Inspection_Lite.csv")
+#write_csv(sample, "Rodent_Inspection_Lite.csv")
+
