1d9a18e1850f76c4b4e170cadfc1cd76d9e6fb40
thuhlz <thuhlz@163.com>
2018-12-10 16:21:24 -0500

    Update Interact.py

--- Interact.py
+++ Interact.py
@@ -154,5 +154,30 @@ def plot():
 		xmax=xmax, xmin=xmin, ymax=ymax, ymin=ymin, width=width, height=height)
 
 
-if __name__ == '__main__':
-	app.run()
+if __name__ == "__main__":
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
+  def run(debug, threaded, host, port):
+    """
+    This function handles command line parameters.
+    Run the server using
+
+        python server.py
+
+    Show the help text using
+
+        python server.py --help
+
+    """
+
+    HOST, PORT = host, port
+    print ("running on %s:%d" % (HOST, PORT))
+    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+
+  run()
